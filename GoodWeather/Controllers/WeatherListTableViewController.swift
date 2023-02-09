df90ce1f0c27ce22bf606952161c37724e319a22
DanielY1108 <scarlet040@gmail.com>
2023-02-09 18:04:36 +0900

    고정값으로 테이블뷰에 지역 및 온도 나오게 설정

--- GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -12,6 +12,33 @@ class WeatherListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+}
+
+extension WeatherListTableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherTableViewCell
+        
+        cell.cityNameLabel.text = "Seoul"
+        cell.temperatureLabel.text = "45°"
+        
+        return cell
     }
 }
+
---
+++ GoodWeather/CustomCells/WeatherTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherTableViewCell.swift
+//  GoodWeather
+//
+//  Created by JINSEOK on 2023/02/09.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
